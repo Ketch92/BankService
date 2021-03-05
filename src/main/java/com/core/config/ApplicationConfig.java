0369faabe6c9b5ee023ab343b1f328c0b9b4d3ba
Oleh Kravets <oleg.kravets92@gmail.com>
2021-03-05 19:35:13 +0200

    Added security configuration

--- src/main/java/com/core/config/ApplicationConfig.java
+++ src/main/java/com/core/config/ApplicationConfig.java
@@ -3,11 +3,18 @@ package com.core.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig {
     @Bean
     public ModelMapper getMapper() {
         return new ModelMapper();
+    }
+    
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/core/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.core.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder encoder;
+    
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          PasswordEncoder encoder) {
+        this.userDetailsService = userDetailsService;
+        this.encoder = encoder;
+    }
+    
+    @Autowired
+    public void globalConfig(AuthenticationManagerBuilder managerBuilder)
+            throws Exception {
+        managerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder);
+    }
+    
+    protected void configure(HttpSecurity security) throws Exception {
+        security.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/orders/complete").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/shopping-carts/movie-sessions").hasRole("USER")
+                .antMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+}
