9352888d057fde7729009a2450e2afac83fd5ef0
Oleh Kravets <okravets@nexway.com>
2021-11-26 22:12:55 +0200

    Cleanup

--- src/main/java/com/core/controller/UserController.java
+++ src/main/java/com/core/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.core.controller;
 
-import com.core.lib.DataProcessingException;
 import com.core.model.User;
 import com.core.model.dto.user.UserRequestDto;
 import com.core.model.dto.user.UserResponseDto;
